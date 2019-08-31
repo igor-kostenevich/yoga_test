6b079e903f90149f8439144f72ca43aac545a4b7
igor-kostenevich <kostenevich9696@mail.ru>
2019-08-31 19:08:44 +0300

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -59,4 +59,14 @@ $(document).ready(function () {
     dots: true
   });
 
+
+  $(document).ready(function(){
+    $("#nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+  });
+
 });
